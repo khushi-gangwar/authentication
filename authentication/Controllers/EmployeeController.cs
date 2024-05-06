b54115942e199ff0de2308ed405e8ea009e186e4
Khushi Gangwar <khushi0425gangwar@gmail.com>
2024-05-07 00:13:23 +0530

    Add project files.

---
+++ authentication/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Authenticated with JWT";
+        }
+        [HttpGet]
+        [Route("Details")]
+        public string Details()
+        {
+            return "Authenticated with JWT";
+        }
+
+    }
+}
---
+++ authentication/Models/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace authentication.Models
+{
+    public class Users
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
